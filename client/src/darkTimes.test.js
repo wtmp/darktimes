d4013a69897789eb0df97743d12062046eb8a0c3
wtmp <wtmp@wtmp.org>
2024-08-06 12:43:09 +0300

    Added telegram library to the client

--- client/src/darkTimes.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import DarkTimes from './DarkTimes';
-
-test('renders learn react link', () => {
-  render(<DarkTimes />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
