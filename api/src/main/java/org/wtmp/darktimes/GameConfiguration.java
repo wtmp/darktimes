049a407682fd130eeb95b702e8079ef7f4a5b15b
wtmp <wtmp@wtmp.org>
2024-08-18 16:32:23 +0300

    added handlers for ssl

--- api/src/main/java/org/wtmp/darktimes/GameConfiguration.java
+++ api/src/main/java/org/wtmp/darktimes/GameConfiguration.java
@@ -3,11 +3,16 @@ package org.wtmp.darktimes;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+
+import javax.net.ssl.SSLException;
+import java.io.File;
 
 @Configuration
 public class GameConfiguration extends AbstractMongoClientConfiguration {
@@ -35,5 +40,19 @@ public class GameConfiguration extends AbstractMongoClientConfiguration {
     @Bean
     public MongoTemplate mongoTemplate(MongoClient mongoClient) {
         return new MongoTemplate(mongoClient, getDatabaseName());
+    }
+
+    @Bean
+    public SslContext sslContext() {
+        try {
+            File keyCertChainFile = new File("./assets/ssl/ssl.crt");
+            File keyFile = new File("./assets/ssl/ssl.key");
+
+            return SslContextBuilder
+                    .forServer(keyCertChainFile, keyFile)
+                    .build();
+        } catch (SSLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- api/src/main/java/org/wtmp/darktimes/server/GameServer.java
+++ api/src/main/java/org/wtmp/darktimes/server/GameServer.java
@@ -6,17 +6,37 @@ import io.netty.channel.*;
 import io.netty.channel.epoll.Epoll;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.internal.ChannelUtils;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.http.HttpContentCompressor;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.util.CharsetUtil;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 import lombok.extern.java.Log;
+import org.springframework.data.util.CastUtils;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Log
 @Component
 public class GameServer {
     private int port = 1982;
+
+    @Resource
+    SslContext sslContext;
 
     @PostConstruct
     public void start() {
@@ -38,12 +58,41 @@ public class GameServer {
                     @Override
                     protected void initChannel(SocketChannel ch) throws Exception {
                         ChannelPipeline pipeline = ch.pipeline();
-                        pipeline.addLast(new ChannelInboundHandlerAdapter() {
+
+                        pipeline.addLast(sslContext.newHandler(ch.alloc()));
+
+                        pipeline.addLast(new HttpServerCodec());
+                        pipeline.addLast(new HttpObjectAggregator(65536));
+                        pipeline.addLast(new HttpContentCompressor());
+                        pipeline.addLast(new WebSocketServerProtocolHandler("/websocket"));
+
+                        pipeline.addLast(new SimpleChannelInboundHandler<WebSocketFrame>() {
                             @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                super.channelRead(ctx, msg);
+                            protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame msg) throws Exception {
+                                TextWebSocketFrame frame = (TextWebSocketFrame)msg;
+
+                                System.out.println(frame.text());
+
+                                ctx.writeAndFlush(new TextWebSocketFrame("huy"));
                             }
                         });
+
+                        //pipeline.addLast(new WebSocketFrameHandler());
+
+//                        pipeline.addLast(new LineBasedFrameDecoder(128));
+//
+//                        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+//
+//                        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+//
+//                        pipeline.addLast(new SimpleChannelInboundHandler<String>() {
+//                            @Override
+//                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+//                                ctx.writeAndFlush("huy");
+//                            }
+//                        });
+
+
                     }
                 })
                 .option(ChannelOption.SO_BACKLOG, 128)
